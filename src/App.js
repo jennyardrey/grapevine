3e61b60a1ca9c357631f1fecb08831536a52ffe5
Jenny Ardrey <jenny@ardrey.co.uk>
2020-02-20 20:54:26 +0000

    project setup

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Route , Switch} from 'react-router-dom';
+import Login from './components/Login';
+import MoodHome from './components/MoodHome';
+import MoodScore from './components/MoodScore';
 
-function App() {
+      function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+			 <Switch>
+				 <Route
+				 exact path="/" component={Login}
+				 />
+				 <Route exact path="/mood-home" component={MoodHome}
+				 />
+				 <Route exact path="/results" component={MoodScore}
+				 />
+			 </Switch>
+
     </div>
   );
 }
-
 export default App;
