824b0ea87dc4d3d3e2e3db15b357104e0c390f4d
GarethBeer <gareth.beer1989@outlook.com>
2020-03-03 20:32:58 +0000

    adding functionality to app so new user is created upon login and state is stored for moodscore and message

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,36 +1,43 @@
 import React from "react";
-import '../styles/LoginDesign.css'
-import image from './images/halftone-yellow/halftone-yellow.png'
+import { Link } from "react-router-dom";
+import "../styles/LoginDesign.css";
+import image from "./images/halftone-yellow/halftone-yellow.png";
 
 const login = props => {
-  const { handleOnSubmit } = props;
+	const { handleOnSubmit } = props;
 
-  return (
-    <div>
-      <h1>Grapevine</h1>
+	return (
+		<div>
+			<h1>Grapevine</h1>
 
-      <img src={image} alt="image" />
+			<img src={image} alt="image" />
 
-      <form className="name" onSubmit={handleOnSubmit}>
-        Name:
-<input className="nameBox" type="text" />
-      </form>
-      <div></div>
+			<form className="name" onSubmit={handleOnSubmit}>
+				Name:
+				<input
+					className="nameBox"
+					type="text"
+					onChange={props.input}
+					name="name"
+				/>
+			</form>
+			<div></div>
 
-      <form className="role" >
-        Role:
-<input className="departmentBox" type="text" />
-
-        <div></div>
-        <button type="submit">
-          Enter
-    </button>
-      </form>
-
-    </div>
-
-  );
-
+			<form className="role">
+				Role:
+				<input
+					className="departmentBox"
+					type="text"
+					onChange={props.input}
+					name="role"
+				/>
+				<div></div>
+				<button type="submit" onClick={props.login}>
+					<Link to="/mood-home">Enter</Link>
+				</button>
+			</form>
+		</div>
+	);
 };
 
 export default login;
--- src/components/MoodHome.js
+++ src/components/MoodHome.js
@@ -1,7 +1,19 @@
-import React from "react";
+import React, { Fragment } from "react";
+import Faces from "./Faces";
+import MoodScore from "./MoodScore";
 
-const moodHome = props => {
-	return <div>MoodHome page</div>;
+const MoodHome = props => {
+	return (
+		<div>
+			{props.faces ? (
+				<MoodScore message={props.message} />
+			) : (
+				<Fragment>
+					<h1>How are you feeling today?</h1>
+					<Faces click={props.click} />
+				</Fragment>
+			)}
+		</div>
+	);
 };
-
-export default moodHome;
+export default MoodHome;
