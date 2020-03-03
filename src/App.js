f70b4c57b27289430bc7e72dd3c5f0ed9c06c04e
GarethBeer <gareth.beer1989@outlook.com>
2020-03-03 19:33:38 +0000

    adding a faces page-needs faces images

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import MoodHome from "./components/MoodHome";
 import MoodScore from "./components/MoodScore";
+import axios from "axios";
 
 class App extends Component {
 	state = {
@@ -16,6 +17,25 @@ class App extends Component {
 		},
 		isLoggedin: false,
 		facesClicked: false
+	};
+
+	loginHandler = () => {
+		const user = {
+			name: this.state.data.name,
+			role: this.state.data.role
+		};
+		axios
+			.post("https://boiling-meadow-46426.herokuapp.com/user", user)
+			.then(response => {
+				const userId = response.data._id;
+				this.setState({
+					data: {
+						...this.state.data,
+						userId: userId
+					},
+					isLoggedIn: true
+				});
+			});
 	};
 
 	render() {
