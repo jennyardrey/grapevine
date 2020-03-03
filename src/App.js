c66db50275e75e2360ed72a3cea8fe4bdaee5b8e
GarethBeer <gareth.beer1989@outlook.com>
2020-03-03 19:22:23 +0000

    adding state to app and creating routing that accept props

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import './App.css';
-import { Route, Switch } from 'react-router-dom';
-import Login from './components/Login';
-import MoodHome from './components/MoodHome';
-import MoodScore from './components/MoodScore';
+import React, { Component } from "react";
+import "./App.css";
+import { Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import MoodHome from "./components/MoodHome";
+import MoodScore from "./components/MoodScore";
 
+class App extends Component {
+	state = {
+		moodData: {
+			userId: null,
+			name: null,
+			role: null,
+			moodScore: null,
+			message: null
+		},
+		isLoggedin: false,
+		facesClicked: false
+	};
 
-function App() {
-	return (
-		<div className="App">
+	render() {
+		return (
+			<div className="App">
+				<Switch>
+					<Route exact path="/" render={props => <Login {...props} />} />
 
-			<Switch>
-				<Route
-					exact path="/" component={Login}
-				/>
-				<Route exact path="/mood-home" component={MoodHome}
-				/>
-				<Route exact path="/results" component={MoodScore}
-				/>
-			</Switch>
-
-		</div>
-	);
+					<Route
+						exact
+						path="/mood-home"
+						render={props => <MoodHome {...props} />}
+					/>
+					<Route exact path="/results" component={MoodScore} />
+				</Switch>
+			</div>
+		);
+	}
 }
 export default App;
