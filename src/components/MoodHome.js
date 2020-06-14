482906b1c48a5b895b4be67cda221560fbbc4c89
Jenny Ardrey <jenny@ardrey.co.uk>
2020-06-14 15:43:45 +0100

    spelling mistakes galore

--- src/components/MoodHome.js
+++ src/components/MoodHome.js
@@ -24,7 +24,7 @@ const MoodHome = props => {
 							<h2>Welcome to Grapevine.</h2>
 							<p>We are dedicated to making your work life as enjoyable and stress free as possible, and believe one way this can be achieved is by better communication.</p>
 							<p>
-								<p>Got feedback for your employer? Feeling annoyed or stressed about something? Want to anonamously let your employer know about something youre concerned about? Or do you simply want to let them know they're doing a great job and you're feeling pretty good?</p>
+								<p>Got feedback for your employer? Feeling annoyed or stressed about something? Want to anonymously let your employer know about something youre concerned about? Or do you simply want to let them know they're doing a great job and you're feeling pretty good?</p>
 								Select the face below that best represents your mood at
 								work at the moment, and you'll have the opportunity to leave feedback (anon or not) for your employer.
 					</p>
