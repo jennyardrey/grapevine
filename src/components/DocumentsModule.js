654230d0af60dd0366141a2a38167800e789c7f5
Jenny Ardrey <jenny@ardrey.co.uk>
2020-06-14 15:22:19 +0100

    added view button to documens page next to download pdf option

--- src/components/DocumentsModule.js
+++ src/components/DocumentsModule.js
@@ -6,12 +6,11 @@ const Documents = () => {
 
 
 	return (
-		<div className="doc-page">
-			<h2 className="doc-intro-text">My Documents</h2>
-			<p className="doc-intro-text">Find all the documents relevant to your employment here. Find answers to questions, check rules in the code of conduct, and see company updates.
+		<div className="doc-module">
+			<h2 className="doc-mod-text">My Documents</h2>
+			<p className="doc-mod-text">Find all the documents relevant to your employment here. Find answers to questions, check rules in the code of conduct, and see company updates.
 		</p>
 			<NavLink to="/documents" className="navlinks">
-
 				<div id="doc-link">View my documents</div>
 			</NavLink>
 		</div>
