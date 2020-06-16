198ee37517e759aa307027421e85dcff44861dee
Jenny Ardrey <jenny@ardrey.co.uk>
2020-06-16 15:35:48 +0100

    restored conditional rendering of faces so they disappear when you click one

--- src/components/Faces.js
+++ src/components/Faces.js
@@ -6,37 +6,48 @@ import face2 from "./images/Face2.png"
 import face3 from "./images/Face3.png"
 import face4 from "./images/Face4.png"
 import face5 from "./images/Face5.png"
+import "../styles/ThankYou.css";
+
 
 const faces = props => {
 	const { click } = props;
 
 	return (
-        <div className={classes['faces-container']}>
-            <h1>If we don't know, we can't help</h1>
-            <div className={classes['step-title']}>
-                <h1>Step One</h1>
-            </div>
-           
-            <p>Select a face that best describes how you are feeling today </p>
-            <div className={classes.Faces}>
-                <button>
-                    <img onClick={click} src={face1} alt='' name='1' />
-                </button>
-                <button>
-                    <img src={face2} alt='' onClick={click} name='2' />
-                </button>
-                <button>
-                    <img src={face3} alt='' onClick={click} name='3' />
-                </button>
-                <button>
-                    <img src={face4} alt='' onClick={click} name='4' />
-                </button>
-                <button>
-                    <img src={face5} alt='' onClick={click} name='5' />
-                </button>
-            </div>
-        </div>
-    );
+		<div className={classes['faces-container']}>
+
+			<h1>If we don't know, we can't help</h1>
+
+			<div className={classes['step-title']}>
+				<h1>Step One</h1>
+			</div>
+
+			<div>
+				{(props.facesClicked === false) ?
+					<div className={classes.Faces}>
+						<p>Select a face that best describes how you are feeling today </p>
+						<button>
+							<img onClick={click} src={face1} alt='' name='1' />
+						</button>
+						<button>
+							<img src={face2} alt='' onClick={click} name='2' />
+						</button>
+						<button>
+							<img src={face3} alt='' onClick={click} name='3' />
+						</button>
+						<button>
+							<img src={face4} alt='' onClick={click} name='4' />
+						</button>
+						<button>
+							<img src={face5} alt='' onClick={click} name='5' />
+						</button>
+					</div> :
+					<div className="thankyou-message"><p>Thank you for your input. Your selection has been recorded and will help your employer keep track of general staff happiness. You'll be able to record your mood again next time, but in the mean time, scroll down to leave more detailed feedback.</p></div>}
+
+			</div>
+
+
+		</div>
+	);
 };
 
 faces.propTypes = {
--- src/components/MoodHome.js
+++ src/components/MoodHome.js
@@ -10,54 +10,54 @@ import ArrowDownwardIcon from '@material-ui/icons/ArrowDownward';
 
 const MoodHome = props => {
 	const { modal, modalClose, name } = props;
-	
-	
-    
-	
-	
+
+
+
+
+
 	const handleClose = (value) => {
 
-        modalClose(); 
-    };
+		modalClose();
+	};
 	return (
-        <div>
-            <Fragment>
-                <WelcomeDialog
-                    open={modal}
-                    onClose={handleClose}
-                    disableBackdropClick={true}
-                    selectedValue={modal}
-                />
+		<div>
+			<Fragment>
+				<WelcomeDialog
+					open={modal}
+					onClose={handleClose}
+					disableBackdropClick={true}
+					selectedValue={modal}
+				/>
 
-                <div className='main-text'>
-                    <h1>Welcome {name}</h1>
-                    <div className='covid-bulitin'>
-                        <h3>Covid-19 Updates</h3>
-                        <p>Due to the current pandemic, lorem50</p>
-                        <DocumentsWidget />
-                    </div>
-                    <div className='begin-prompt'>
-                        <h1>Let's Begin</h1>
-                        <ArrowDownwardIcon className='arrow'/>
-                    </div>
-                </div>
-                <Faces click={props.click} />
-                <SendMessage
-                    message={props.message}
-                    send={props.submitMessage}
-                    toggle={props.toggle}
-                    anon={props.anon}
-                    messageSent={props.messageSent}
-                />
-                <div className='step-3'>
-                    <div className='step-title'>
-                        <h1>Step Three</h1>
-                    </div>
-                </div>
-            </Fragment>
+				<div className='main-text'>
+					<h1>Welcome {name}</h1>
+					<div className='covid-bulitin'>
+						<h3>Covid-19 Updates</h3>
+						<p>Due to the current pandemic, lorem50</p>
+						<DocumentsWidget />
+					</div>
+					<div className='begin-prompt'>
+						<h1>Let's Begin</h1>
+						<ArrowDownwardIcon className='arrow' />
+					</div>
+				</div>
+				<Faces click={props.click} facesClicked={props.faces} />
+				<SendMessage
+					message={props.message}
+					send={props.submitMessage}
+					toggle={props.toggle}
+					anon={props.anon}
+					messageSent={props.messageSent}
+				/>
+				<div className='step-3'>
+					<div className='step-title'>
+						<h1>Step Three</h1>
+					</div>
+				</div>
+			</Fragment>
 
-            <Footer />
-        </div>
-    );
+			<Footer />
+		</div>
+	);
 };
 export default MoodHome;
