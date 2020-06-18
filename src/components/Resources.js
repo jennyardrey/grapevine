1367b0c6a803e7305f5a0fa09fa2f1db88845880
Jenny Ardrey <jenny@ardrey.co.uk>
2020-06-18 11:38:30 +0100

    completed ease in type transitions on resources section using react transition group

--- src/components/Resources.js
+++ src/components/Resources.js
@@ -45,13 +45,32 @@ class Resources extends Component {
 					</div>
 				</div>
 				<CSSTransition
-					in={this.state.talk || this.state.meditate || this.state.music || this.state.call}
+					in={this.state.talk}
 					timeout={350}
 					classNames="resource-animation"
-					unmountOnExit>
-					<div className="resource-content">
-						{(this.state.talk) ? <p>Sometimes we just need to talk it out! Find someone at work who can sympathise with your concerns, or find someone at home who can lend an ear whilst you get some things off your chest. Articulating your worries can sometimes help understand them better, or perhaps even let them go.</p> : (this.state.music) ? <p>Listening to music can be an extremely effective stress-management tool. Why not try these relaxing playlists on Spotify, or put on your favourite jams and kick back.</p> : (this.state.meditate) ? <p>Meditating doesnt have to be reserved for a serene moment by a lake in the forest, and isnt just for yogis. You can meditate anywhere, and sometimes it's helpful to take 5 or 10 minutes out of a busy day to collect your thoughts, and find a bit of calm in the chaos. There's lots of apps on the market to help people meditate, but we like Headspace. Follow the link for information about meditating for beginners, incuding a free 11 minute meditation you can try right now.</p> : (this.state.call) ? <p>More often than not, if something is bothering you at work, your direct leaders will be able to help. In a situation where you can't talk to someone at work, there are helplines where you can call anoymously and get advice from trained professionals. Find out more information about those here.</p> : null}
-					</div>
+				>
+					<CSSTransition
+						in={this.state.music}
+						timeout={350}
+						classNames="resource-animation"
+					>
+						<CSSTransition
+							in={this.state.meditate}
+							timeout={350}
+							classNames="resource-animation"
+						>
+							<CSSTransition
+								in={this.state.call}
+								timeout={350}
+								classNames="resource-animation"
+							>
+
+								<div className="resource-content">
+									{(this.state.talk) ? <p>Sometimes we just need to talk it out! Find someone at work who can sympathise with your concerns, or find someone at home who can lend an ear whilst you get some things off your chest. Articulating your worries can sometimes help understand them better, or perhaps even let them go.</p> : (this.state.music) ? <p>Listening to music can be an extremely effective stress-management tool. Why not try these relaxing playlists on Spotify, or put on your favourite jams and kick back.</p> : (this.state.meditate) ? <p>Meditating doesnt have to be reserved for a serene moment by a lake in the forest, and isnt just for yogis. You can meditate anywhere, and sometimes it's helpful to take 5 or 10 minutes out of a busy day to collect your thoughts, and find a bit of calm in the chaos. There's lots of apps on the market to help people meditate, but we like Headspace. Follow the link for information about meditating for beginners, incuding a free 11 minute meditation you can try right now.</p> : (this.state.call) ? <p>More often than not, if something is bothering you at work, your direct leaders will be able to help. In a situation where you can't talk to someone at work, there are helplines where you can call anoymously and get advice from trained professionals. Find out more information about those here.</p> : null}
+								</div>
+							</CSSTransition>
+						</CSSTransition>
+					</CSSTransition>
 				</CSSTransition>
 			</div>
 		);
